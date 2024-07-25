1753d79de52aa9a9b74b0ac7305a5fc985171900
scriubuguri <scriubuguri@tragdate.ninja>
2024-07-25 19:25:14 +0000

    update CSS and HTML for form centering

--- shekelworth/shekelworth/settings.py
+++ shekelworth/shekelworth/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+        BASE_DIR / "shekelworthapp/static"
+        ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
