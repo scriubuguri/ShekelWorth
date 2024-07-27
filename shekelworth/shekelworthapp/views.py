4083d5bbfcdebadf0110aed4f7996ebd8b6eb5c9
scriubuguri <scriubuguri@tragdate.ninja>
2024-07-27 11:12:39 +0000

    refactor to display result on the same page

--- shekelworth/shekelworthapp/views.py
+++ shekelworth/shekelworthapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.http import HttpResponse
 from .forms import CurrencyForm
 import requests
@@ -9,37 +8,22 @@ import json
 
 def shekel_worth(request):
     forms = CurrencyForm()
+    conversion_result = None
+
     if request.POST:
         data = request.POST.dict()
-        request.session['our_data'] = data
-        return redirect('result')
-    return render(request, 'index.html', {'forms': forms})
+        _from = data['From']
+        to = data['To']
+        amount = data['amount']
 
-def result(request):
+        # Request parameters
+        MAIN_URL = "https://v6.exchangerate-api.com/v6/"
+        API_KEY = ""
+        FULL_URL = f"{MAIN_URL}{API_KEY}/pair/{_from}/{to}/{amount}/"
 
-    #request parameters
-    MAIN_URL = "https://v6.exchangerate-api.com/v6/"
-    API_KEY = "your_api_key"
+        response = requests.get(FULL_URL)
+        resp = response.json()
+        res = resp['conversion_result']
+        conversion_result = f"{amount} {_from} is equivalent to {res} {to}"
 
-    data = request.session['our_data']
-    _from = data['From']
-    to = data['To']
-    amount = data['amount']
-    FULL_URL = "{main_url}{api_key}/pair/{_from}/{to}/{amount}/".format(
-       main_url=MAIN_URL,
-       api_key=API_KEY,
-       _from=_from,
-       to=to,
-       amount=amount
-    )
-
-    response = requests.get(FULL_URL)
-    resp = response.json()
-    res = resp['conversion_result']
-    conversion_result = "Amount {amount} {_from} to {to} is: {result}".format(
-        amount=amount,
-        _from=_from,
-        to=to,
-        result=res
-    )
-    return HttpResponse(conversion_result)
+    return render(request, 'index.html', {'forms': forms, 'conversion_result': conversion_result})
